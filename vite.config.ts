dcc07302a8e882bb8a396395a60319c1c97d4ec5
qq563986150 <85390190+qq563986150@users.noreply.github.com>
2025-09-11 16:17:33 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import IdentityCard from '@/components/IdentityCard.tsx'
+import SpiritRootModal from '@/components/SpiritRootModal.tsx'
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
